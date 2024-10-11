ea840b5beb31d7c455434c04ca643c37d1b4a372
Helmi azkia <azkiasekian@gmail.com>
2024-10-11 08:21:36 +0700

    menambahkan font google

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 function App() { 
   return (
 	  <>
-	    <h1 className="text-3xl font-bold underline">
-	      Hello world!
+	    <h1 className="text-3xl font-bold">
+	      Ini font body
 	    </h1>
+      <h1 className="text-3xl font-poppins">
+	      Ini font poppins
+	    </h1>
+      <h1 className="text-3xl">
+	      Ini font body
+	    </h1>
+      <p>
+        ini font body jua. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum consequatur odio temporibus, sed eveniet expedita! Nisi dolorem blanditiis a corrupti cumque, odio alias laudantium voluptates, fugiat optio eligendi modi repellat.
+      </p>
 	  </>
   )
 }
