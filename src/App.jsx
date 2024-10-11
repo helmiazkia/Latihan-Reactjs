f8f54d3fab61753b956da06077168c74db6840fa
Helmi azkia <azkiasekian@gmail.com>
2024-10-11 08:29:20 +0700

    Penambahan warna

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 function App() { 
   return (
 	  <>
-	    <h1 className="text-3xl font-bold">
+	    <h1 className="text-3xl font-bold text-secondary">
 	      Ini font body
 	    </h1>
-      <h1 className="text-3xl font-poppins">
+      <h1 className="text-3xl font-poppins text-primary">
 	      Ini font poppins
 	    </h1>
-      <h1 className="text-3xl">
+      <h1 className="text-3xl bg-slate-700">
 	      Ini font body
 	    </h1>
       <p>
