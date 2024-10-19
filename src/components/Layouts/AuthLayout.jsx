659d1a331eb8901c961751b50b4ba1900cab7988
Helmi azkia <azkiasekian@gmail.com>
2024-10-20 06:25:11 +0700

    menambahkan vercel json

--- src/components/Layouts/AuthLayout.jsx
+++ src/components/Layouts/AuthLayout.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Logo from "../Elements/Logo";
 
 const Authlayout = (props) => {
@@ -92,10 +93,10 @@ const Authlayout = (props) => {
               <span className="text-sm text-gray-03">
                 Already have an account?&nbsp;
               </span>
-              <a href="/login" className="text-primary text-sm font-bold">Sign In Here</a>
+              <Link to="/login" className="text-primary text-sm font-bold">Sign In Here</Link>
             </>
           ) : (
-            <a href="/register" className="text-primary text-sm font-bold">Create an account</a>
+            <Link to="/register" className="text-primary text-sm font-bold">Create an account</Link>
           )}
         </div>
         {/* link end */}
