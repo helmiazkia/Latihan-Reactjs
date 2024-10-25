ebe537c035d7c797c1116f1f137719b8a038ddea
Helmi azkia <azkiasekian@gmail.com>
2024-10-26 01:15:39 +0700

    Merapihkan forgot password

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,26 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignInPage from "./pages/signIn";
 import SignUpPage from "./pages/signUp";
 import ErrorRoute from "./pages/errorRoute";
-
+import ForgotPassword from "./pages/ForgotPassword";  // Changed to uppercase F
 
 const App = () => {
   const myRouter = createBrowserRouter([
     {
       path: "/",
       element: <div>Halaman Utama</div>,
-      errorElement: <ErrorRoute/ >
+      errorElement: <ErrorRoute />,
     },
     {
       path: "/login",
-      element: <SignInPage/>,
+      element: <SignInPage />,
     },
     {
       path: "/register",
-      element: <SignUpPage/>,
+      element: <SignUpPage />,
+    },
+    {
+      path: "/forgot-password",
+      element: <ForgotPassword />  // Changed to uppercase F
     },
   ]);
 
