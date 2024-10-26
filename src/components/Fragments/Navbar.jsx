360a9aa0017558c3651b3c40f6635e01e16fb681
Helmi azkia <azkiasekian@gmail.com>
2024-10-26 14:51:20 +0700

    react icon

--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -1,45 +1,61 @@
 import { Link } from "react-router-dom";
+import { Icon } from "../Elements/Icon";
 
 const Navbar = () => {
+    const menus = [
+        {
+          id: "overview",
+          link: "/",
+          icon: <Icon.Overview />,
+          label: "Overview",
+        },
+        {
+          id: "balance",
+          link: "/balance",
+          icon: <Icon.Balance />,
+          label: "Balance",
+        },
+        {
+          id: "transaction",
+          link: "/transaction",
+          icon: <Icon.Transaction />,
+          label: "Transaction",
+        },
+      ];
+
     return(
-        <nav className="bg-defaultBlack text-special-bg2 sm:w-72 w-36 h-screen px-7 py-12 flex flex-col justify-between">
-        <div>
-            <Link to="/">
-            <div className="flex justify-center mb-10">Logo</div>
-            <div className="flex bg-primary text-white px-4 py-3 rounded-md">
-            <div className="text-white mx-auto sm:mx-0">A</div>
-            <div className="ms-3 font-bold text-white hidden sm:block">Overview</div>
+         <nav className="bg-defaultBlack text-special-bg2 sm:w-72 w-36 min-h-screen px-7 py-12 flex flex-col justify-between">
+      <div>
+        <div className="flex justify-center mb-10">Logo</div>
+        {menus.map((menu) => (
+          <Link to={menu.link}>
+            <div className="flex hover:bg-special-bg3 hover:text-white px-4 py-3 rounded-md">
+              <div className="mx-auto sm:mx-0">{menu.icon}</div>
+              <div className="ms-3 hidden sm:block">{menu.label}</div>
             </div>
-            </Link>
-            <Link to="/balance">
-            <div className="flex hover:bg-special-bg3 px-4 py-3 rounded-md">
-            <div className="mx-auto sm:mx-0">B</div>
-            <div className="ms-3 text-special-bg2 hidden sm:block">Balances</div>
-            </div>
-            </Link>
-            
-            <div className="flex hover:bg-special-bg3 px-4 py-3 rounded-md">
-            <div className="mx-auto sm:mx-0">C</div>
-            <div className="ms-3 text-special-bg2 hidden sm:block">Transactions</div>
-            </div>
+          </Link>
+        ))}
+      </div>
+      <div className="sticky bottom-12">
+	      <Link to="/logout">
+	        <div className="flex bg-special-bg3 px-4 py-3 rounded-md hover:text-white">
+	          <div className="mx-auto sm:mx-0">D</div>
+	          <div className="ms-3 hidden sm:block">Logout</div>
+	        </div>
+        </Link>
+        <div className="border-b my-10 border-b-special-bg"></div>
+        <div className="flex justify-between">
+          <div className="mx-auto sm:mx-0">foto</div>
+          <div className="hidden sm:block">
+            Username
+            <br />
+            View Profile
+          </div>
+          <div className="hidden sm:block">icon</div>
         </div>
-        <div className="sticky bottom-12">
-            <div className="flex bg-special-bg3 px-4 py-3 rounded-md">
-            <div className="mx-auto sm:mx-0">D</div>
-            <div className="ms-3 text-special-bg2 hidden sm:block">Logout</div>
-            </div>
-            <div className="border-b my-10 border-b-special-bg"></div>
-            <div className="flex justify-between">
-            <div className="mx-auto sm:mx-0">foto</div>
-            <div className="hidden sm:block">
-                Username
-                <br />
-                View Profile
-            </div>
-            <div className="hidden sm:block">icon</div>
-            </div>
-        </div>
-        </nav>
-    );
+      </div>
+    </nav>
+  );
 };
+
 export default Navbar;
