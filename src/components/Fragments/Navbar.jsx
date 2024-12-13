77ff89feb16def184f8d6f0ddb1359bca8e66182
Helmi azkia <azkiasekian@gmail.com>
2024-12-13 08:35:36 +0700

    State management belum fix

--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -1,8 +1,19 @@
 import { Link, NavLink } from "react-router-dom";
 import { Icon } from "../Elements/Icon"; // Pastikan file dan komponen Icon ada
 import Logo from "../Elements/Logo";
+import { useState } from "react";
 
 const Navbar = () => {
+  const themes = [
+    { name: "theme-green", bgcolor: "bg-[#299D91]", color: "#299D91" },
+    { name: "theme-blue", bgcolor: "bg-[#1E90FF]", color: "#1E90FF" },
+    { name: "theme-purple", bgcolor: "bg-[#6A5ACD]", color: "#6A5ACD" },
+    { name: "theme-pink", bgcolor: "bg-[#DB7093]", color: "#DB7093" },
+    { name: "theme-brown", bgcolor: "bg-[#8B4513]", color: "#8B4513" },
+  ];
+
+  const [theme, setTheme] = useState(themes[0]);
+
   const menus = [
     {
       id: "overview",
@@ -37,7 +48,7 @@ const Navbar = () => {
     {
       id: "goals",
       link: "/goals",
-      icon: <Icon.Goal/>,
+      icon: <Icon.Goal />,
       label: "Goals",
     },
     {
@@ -49,7 +60,7 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="bg-defaultBlack">
+    <div className={`bg-defaultBlack ${theme.name}`}>
       <nav className="sticky top-0 text-special-bg2 sm:w-72 w-28 min-h-screen px-7 py-12 flex flex-col justify-between">
         <div>
           <NavLink to="/" className="flex justify-center mb-10">
@@ -70,12 +81,22 @@ const Navbar = () => {
             </NavLink>
           ))}
         </div>
+        <div className="md:flex md:gap-2">
+          Themes
+          {themes.map((t) => (
+            <div
+              key={t.name}
+              className={`${t.bgcolor} md:w-6 h-6 rounded-md cursor-pointer mb-2`}
+              onClick={() => setTheme(t)}
+            ></div>
+          ))}
+        </div>
         <div>
           <NavLink
-            to="/logout"
+            to="/login"
             className="flex bg-special-bg3 px-4 py-3 rounded-md hover:text-white"
           >
-            <div className="mx-auto sm:mx-0">
+            <div className="mx-auto sm:mx-0 text-primary">
               <Icon.Logout />
             </div>
             <div className="ms-3 hidden sm:block">Logout</div>
@@ -98,6 +119,5 @@ const Navbar = () => {
     </div>
   );
 };
-
 
 export default Navbar;
